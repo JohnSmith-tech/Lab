0172d33bf8967c5f35342028e99dbdd32e2f2c86
JohnSmith-Tech <iea2001@mail.ru>
2021-12-08 16:17:38 +0700

    Implementation of the Dijkstra and Bellman-Ford algorithms

--- Graph/src/Graph.java
+++ Graph/src/Graph.java
@@ -31,19 +31,24 @@ public class Graph {
         if (start >= verticesCount) {
             return null;
         }
+
         List<Integer> d = new ArrayList<>();
         List<Integer> temp = new ArrayList<>();
+
         for (int i = 0; i < verticesCount; i++) {
             d.add(Integer.MAX_VALUE);
         }
+
         d.set(start, 0);
         boolean flag = true;
-        int step = 1;
+
         while (flag) {
             int checked = 0;
+
             for (int i = 0; i < verticesCount; i++) {
                 if (i == start)
                     continue;
+
                 for (int j = 0; j < verticesCount; j++) {
                     if (matrix[j][i] == Integer.MAX_VALUE || d.get(j) == Integer.MAX_VALUE) {
                         temp.add(Integer.MAX_VALUE);
@@ -51,15 +56,18 @@ public class Graph {
                         temp.add(d.get(j) + matrix[j][i]);
                     }
                 }
+
                 int minElementInTemp = searchMinElement(temp);
+
                 if (!Objects.equals(d.get(i), minElementInTemp)) {
                     checked = 0;
                 } else {
                     checked += 1;
                 }
+
                 d.set(i, minElementInTemp);
                 temp.clear();
-                step += 1;
+
                 if (checked == verticesCount - 1)
                     flag = false;
             }
@@ -72,16 +80,20 @@ public class Graph {
         if (start >= verticesCount) {
             return null;
         }
+
         int[] d = new int[verticesCount];
         boolean[] visited = new boolean[verticesCount];
+
         for (int i = 0; i < verticesCount; i++) {
             d[i] = Integer.MAX_VALUE;
             visited[i] = false;
         }
         d[start] = 0;
+
         for (int i = 0; i < verticesCount - 1; i++) {
             int index = searchMinIndex(d, visited);
             visited[index] = true;
+
             for (int v = 0; v < verticesCount; v++)
                 if (matrix[index][v] != Integer.MAX_VALUE && d[index] != Integer.MAX_VALUE && !visited[v])
                     d[v] = Math.min(d[v], d[index] + matrix[index][v]);
@@ -92,6 +104,7 @@ public class Graph {
 
     public static int searchMinElement(List<Integer> array) {
         int minElement = Integer.MAX_VALUE;
+
         for (Integer integer : array) {
             if (integer < minElement) {
                 minElement = integer;
@@ -104,6 +117,7 @@ public class Graph {
     public static int searchMinIndex(int[] array, boolean[] visited) {
         int index = 0;
         int minElement = Integer.MAX_VALUE;
+
         for (int i = 0; i < array.length; i++) {
             if (array[i] < minElement && !visited[i]) {
                 minElement = array[i];
